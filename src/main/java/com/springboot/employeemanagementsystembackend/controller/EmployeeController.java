0845d9f17114b8f9311240c6c0400d87876a0c0f
Denisa Andra Ionitoiu <denisa.andra.ionitoiu@ibm.com>
2023-10-19 15:41:06 +0300

    Created update employee by id REST api.

--- src/main/java/com/springboot/employeemanagementsystembackend/controller/EmployeeController.java
+++ src/main/java/com/springboot/employeemanagementsystembackend/controller/EmployeeController.java
@@ -23,16 +23,34 @@ public class EmployeeController {
     }
 
     // Create employee
-    @PostMapping("/employees")
+    @PostMapping("/employees") // We use @RequestBody because the request contains a JSON request body and we want to map it directly to the employee object.
     public Employee createEmployee(@RequestBody Employee employee) { //POST request contains a JSON request body so @RequestBody maps the JSON request body to the Employee object
         return employeeRepository.save(employee);
     }
 
-    // get Employee by id
-    @GetMapping("/employees/{id}") // We want to store this id value into the Java variable, so for that we are going to use @PathVariable annotation to map the url {id} into the variable
+    // Get Employee by id
+    @GetMapping("/employees/{id}") // We want to store this path id value into the Java variable, so for that we are going to use @PathVariable annotation to map the url {id} into the variable
     public ResponseEntity<Employee>  getEmployeeById(@PathVariable Long id) {
-        Employee employee= employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+        Employee employee= employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with this id: " + id));
 
         return ResponseEntity.ok(employee);
     }
+
+    // Update Employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee existingEmployee = this.employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with this id: " + id));
+        existingEmployee.setfirstName(employeeDetails.getfirstName());
+        existingEmployee.setLastName(employeeDetails.getLastName());
+        existingEmployee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updatedEmployee = this.employeeRepository.save(existingEmployee);
+
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+
+
+
 }
+
