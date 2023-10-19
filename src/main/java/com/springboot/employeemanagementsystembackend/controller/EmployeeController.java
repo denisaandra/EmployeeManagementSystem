0763d9d8db2a04ef5ebd2d19e0e77281dfbb2db6
Denisa Andra Ionitoiu <denisa.andra.ionitoiu@ibm.com>
2023-10-19 14:50:00 +0300

    Created get employee by id REST api.

--- src/main/java/com/springboot/employeemanagementsystembackend/controller/EmployeeController.java
+++ src/main/java/com/springboot/employeemanagementsystembackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.springboot.employeemanagementsystembackend.controller;
 
+import com.springboot.employeemanagementsystembackend.exception.ResourceNotFoundException;
 import com.springboot.employeemanagementsystembackend.model.Employee;
 import com.springboot.employeemanagementsystembackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) { //POST request contains a JSON request body so @RequestBody maps the JSON request body to the Employee object
         return employeeRepository.save(employee);
+    }
+
+    // get Employee by id
+    @GetMapping("/employees/{id}") // We want to store this id value into the Java variable, so for that we are going to use @PathVariable annotation to map the url {id} into the variable
+    public ResponseEntity<Employee>  getEmployeeById(@PathVariable Long id) {
+        Employee employee= employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+
+        return ResponseEntity.ok(employee);
     }
 }
